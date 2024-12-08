b14e5f5bf31367fc7f59f217628776d675a6d018
Darren Ogola <darren.sunday@strathmore.edu>
2024-12-08 22:14:43 +0300

    My day42 of 365 challange

---
+++ day42.py
@@ -0,0 +1,7 @@
+email=input("Enter your Email:")
+
+email=email.strip()
+slicer_index=email.index("@")
+username=email[:slicer_index]
+domain_name=email[slicer_index+1:]
+print("Your user name is",username,"and your domain is",domain_name)
